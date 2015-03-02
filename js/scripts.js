2e6048048db0d9d9d44f79dd4ef881d74e475048
Sean Holstrom <basementbebop@gmail.com>
2015-03-01 17:08:16 -0800

    Add update longitude/latitude functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   // JSON calls
   var apiKey = '70433fafb74c3e90db90f265a6c0dced';
   var url = 'https://api.forecast.io/forecast/';
-  var lati = 45.5200;
-  var longi = -122.6819;
+  var latit = 45.5200;
+  var longit = -122.6819;
   var data;
   function getWeather() {
-    // $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-    //   $('#precip').html(data.currently.precipProbability * 100);
-    //   $('#temp').html(data.currently.temperature);
-    // });
+    $.getJSON(url + apiKey + "/" + latit + "," + longit + "?callback=?", function(data) {
+      $('#precip').html(data.currently.precipProbability * 100);
+      $('#temp').html(data.currently.temperature);
+    });
   }
   getWeather();
 
   // Refresh button
-  $('form').submit(function (evt) {
+  $('#refresh-weather').submit(function (evt) {
     evt.preventDefault();
     getWeather();
   })
@@ -22,14 +22,31 @@ $(document).ready(function() {
   // Side nav
   var $menuReveal = $('.menu-reveal');
   var $sideNavMask = $('.side-nav-mask');
+  var $sideNav = $('.side-nav');
+  var $refreshBtn = $('.refresh-btn');
 
   $menuReveal.on('click', function() {
     $sideNavMask.addClass('visible');
+    $sideNav.addClass('visible');
+    $refreshBtn.addClass('visible');
     $menuReveal.removeClass('visible');
-  });
+    });
 
   $sideNavMask.on('click', function() {
     $sideNavMask.removeClass('visible');
+    $sideNav.removeClass('visible');
+    $refreshBtn.removeClass('visible');
+    $menuReveal.addClass('visible');
+  });
+
+  $('#refresh-location').submit(function(evt) {
+    evt.preventDefault();
+    latit = $("#lati").val();
+    longit = $("#longi").val();
+    getWeather();
+    $sideNavMask.removeClass('visible');
+    $sideNav.removeClass('visible');
+    $refreshBtn.removeClass('visible');
     $menuReveal.addClass('visible');
   });
 
